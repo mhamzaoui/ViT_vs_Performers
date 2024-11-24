c82b2222b7429d955bd036bca2750c71299f4aa3
guillaumechl <guillaume.chilla@columbia.edu>
2024-11-24 15:12:01 -0500

    add of the data + correction on performers

--- src/Performers/performers_pytorch.py
+++ src/Performers/performers_pytorch.py
@@ -10,7 +10,7 @@ from contextlib import contextmanager
 
 from local_attention import LocalAttention
 from axial_positional_embedding import AxialPositionalEmbedding
-from performer_pytorch.reversible import ReversibleSequence, SequentialSequence
+from reversible import ReversibleSequence, SequentialSequence
 
 from distutils.version import LooseVersion
 
